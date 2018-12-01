27636d1cd5a1e93dccbd42fa90c5d5b05a41958e
TheStar <sk_and_mc@kaist.ac.kr>
2018-12-01 13:00:58 +0000

    find first al

---
+++ basic/DS_1/10809_first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// '\n' ""
+
+int main(){
+	int n[26];
+	fill_n(n, 26, -1);
+	string s;
+	cin >> s;
+	
+	for(int i=0;i<s.size();i++){
+		if (n[int(s[i]-97)] == -1) n[int(s[i]-97)] = i;
+	}
+	for(int i=0;i<26;i++) cout << n[i] << " ";
+}
