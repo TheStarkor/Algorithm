00ea8cf8251d5c420ca698fd187ab2cf9f831afe
TheStar <sk_and_mc@kaist.ac.kr>
2018-12-18 14:41:12 +0000

    1699

--- basic/DP_1/1699.cpp
+++ basic/DP_1/1699.cpp
@@ -7,7 +7,7 @@ int main(){
 	for (int i=1;i<=n;i++){
 		d[i]=i;
 		for (int j=1;j*j<=i;j++){
-			if ((1+d[i-j*j])<d[i]) d[i] = 1 + d[i-j*j];
+			if (1+d[i-j*j] < d[i]) d[i] = 1 + d[i-j*j];
 		}
 	}
 	cout << d[n];
